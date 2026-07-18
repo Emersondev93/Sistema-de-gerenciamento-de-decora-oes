52953b9d23752ebc56df454248687c0a182da6f3
Emerson Amorim <emersonamorim93@gmail.com>
2026-07-18 12:26:07 -0300

    Implementações na classe sistema e criação da classe Main. Em desenvolvimento...

---
+++ src/aplicacao/Main.java
@@ -0,0 +1,45 @@
+package aplicacao;
+
+import servicos.Sistema;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 - Cadastrar cliente" +
+                "\n2 - Listar clientes" +
+                "\n3 - Cadastrar evento" +
+                "\n4 - Listar eventos" +
+                "\n5 -  Sair");
+
+        Sistema sistema = new Sistema();
+        int opcao;
+        do {
+            opcao = sc.nextInt();
+            sc.nextLine();
+            switch (opcao) {
+                case 1:
+                    sistema.cadastrarCliente();
+                    break;
+                case 2:
+                    sistema.listarClientes();
+                    break;
+                case 3:
+                    sistema.cadastrarEvento();
+                    break;
+                case 4:
+                    sistema.listarEvento();
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+
+        } while (opcao != 5);
+    }
+}
--- src/servicos/Sistema.java
+++ src/servicos/Sistema.java
@@ -17,11 +17,10 @@ public class Sistema {
     private List<Cliente> clientes = new ArrayList<>();
     private List<Evento> eventos = new ArrayList<>();
     DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    int idCliente = 1;
 
     public void cadastrarCliente() {
         System.out.println("CADASTRO ");
-        int idCliente = 1;
-        sc.nextLine();
         System.out.print("Nome: ");
         String nome = sc.nextLine();
         System.out.print("Telefone:");
@@ -30,7 +29,7 @@ public class Sistema {
         String rua = sc.nextLine();
         System.out.print("Número: ");
         String numero = sc.nextLine();
-        System.out.println("Bairro: ");
+        System.out.print("Bairro: ");
         String bairro = sc.nextLine();
         System.out.print("Cidade: ");
         String cidade = sc.nextLine();
@@ -39,15 +38,22 @@ public class Sistema {
         Endereco endereco = new Endereco(rua, numero, bairro, cidade, cep);
         Cliente cliente = new Cliente(idCliente, nome, telefone, endereco);
         clientes.add(cliente);
+        System.out.println("Cliente cadastrado com sucesso! ID: " + idCliente);
         idCliente++;
-
     }
 
     public List<Cliente> listarClientes() {
         return new ArrayList<>(clientes);
     }
 
-    public void buscarCliente() {
+    private void buscarEExibirCliente() {
+        Cliente cliente = buscaPorMenu();
+        if (cliente != null) {
+            System.out.println(cliente);
+        }
+    }
+
+    public Cliente buscaPorMenu() {
         System.out.println("Digite o número correspondente ao tipo de busca: ");
         System.out.println("1 - Busca por ID.\n2 - Busca por nome. \n3 - Busca por telefone.");
         System.out.print("Opção: ");
@@ -57,35 +63,37 @@ public class Sistema {
             case 1:
                 System.out.print("Digite o ID: ");
                 int idBusca = sc.nextInt();
+                sc.nextLine();
                 for (Cliente cliente : clientes) {
                     if (idBusca == cliente.getId()) {
-                        System.out.println(cliente);
+                        return cliente;
                     }
-                    break;
                 }
+                break;
             case 2:
                 System.out.print("Digite o nome: ");
                 String nomeBusca = sc.nextLine();
                 for (Cliente cliente : clientes) {
                     if (nomeBusca.equals(cliente.getNome())) {
-                        System.out.println(cliente);
+                        return cliente;
                     }
-                    break;
                 }
+                break;
             case 3:
                 System.out.print("Digite o telefone");
                 String telefoneBusca = sc.nextLine();
                 for (Cliente cliente : clientes) {
                     if (telefoneBusca.equals(cliente.getTelefone())) {
-                        System.out.println(cliente);
+                        return cliente;
                     }
-                    break;
                 }
+                break;
             default:
                 System.out.println("Tipo de busca inválido.");
 
         }
-
+        System.out.println("Cliente não encontrado!");
+        return null;
     }
 
     public void removerCliente(Cliente cliente) {
@@ -94,13 +102,31 @@ public class Sistema {
         clientes.removeIf(c -> c.getId() == idRemover);
     }
 
-    public void cadastrarEvento(){
-        String idEvento = "ev" + 001 + new Random(999);
-        System.out.print("Data: ");
+    public void cadastrarEvento() {
+        System.out.println("Contratante: ");
+        Cliente cliente = buscaPorMenu();
+        if (cliente == null) {
+            System.out.println("Erro! Cliente não encontrado no cadastro.");
+            return;
+        }
+        int numAleatorio = random.nextInt(1000);
+        String idEvento = "ev" + numAleatorio;
+        System.out.print("Data (dd/MM/aaaa): ");
         String data = sc.next();
         LocalDate dataFormatada = LocalDate.parse(data, fmt);
+        System.out.print("Tema: ");
+        String tema = sc.nextLine();
+        System.out.print("Valor: ");
+        double valor = sc.nextDouble();
 
-
+        Evento novoEvento = new Evento(idEvento, dataFormatada, tema, valor, cliente);
+        eventos.add(novoEvento);
+        System.out.println("Evento " + idEvento + " cadastrado com sucesso ");
     }
-
+    public List<Evento> listarEvento(){return new ArrayList<>(eventos);}
+    public void removerEvento(Evento evento){
+        System.out.print("Digite o ID o evento: ");
+        String idRemover = sc.next();
+        eventos.removeIf(e -> e.getIdEvento().equals(idRemover));
+    }
 }
